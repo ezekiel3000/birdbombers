11d8d49042436cabce163f75cc57a17a8ed6a6a1
Ezekiel <ejframe3000@gmail.com>
2012-04-11 13:35:23 -0400

    loading of resourses

---
+++ birdPoopTestingGround/gen/com/ezekiel/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ezekiel;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ birdPoopTestingGround/src/com/ezekiel/BirdPoopTestingGroundActivity.java
@@ -0,0 +1,30 @@
+package com.ezekiel;
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+
+
+public class BirdPoopTestingGroundActivity extends BaseGameActivity{
+
+	public void onLoadComplete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Engine onLoadEngine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void onLoadResources() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Scene onLoadScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
